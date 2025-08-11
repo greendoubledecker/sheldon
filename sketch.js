e503475e3dfafa15adfbac15c46768969f01babd
greendoubledecker <95518384+greendoubledecker@users.noreply.github.com>
2025-08-11 17:46:27 +0800

    Add the files.

---
+++ sketch.js
@@ -0,0 +1,17 @@
+let x = 200;
+let y = 200;
+let w = 500;
+let h = 300;
+
+function setup() {
+  createCanvas(800, 800);
+  background(0, 255, 175);
+  strokeWeight(50);
+  stroke(255, 130, 0);
+  fill(180, 10, 10);
+  rect(x, y, w, h);
+}
+
+// function draw() {
+//   background(220);
+// }
